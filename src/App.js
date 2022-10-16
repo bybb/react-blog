7d63a5e71e4b31092980283bb47f9b711c71e070
oned-main <ai9gle@gmail.com>
2022-10-16 16:07:26 +0900

    add modal component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코드 추천', '강남 우동맛집', '파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>{ post }</h4>
+        <h4>ReactBlog</h4>
       </div>
+      <button onClick={() => {
+        let sortData = [...글제목].sort();
+        글제목변경(sortData);
+      }}>가나다라순정렬</button>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
@@ -24,6 +32,19 @@ function App() {
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal/>
+
+    </div>
+  );
+}
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
